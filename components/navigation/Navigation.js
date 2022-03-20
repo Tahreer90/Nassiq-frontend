809ec2206f7dcc5710e5c98ab9d2e75a01304f1d
fatemah-alm <fatemah.alm@hotmail.com>
2022-03-20 22:11:24 +0300

    sign in and sign up

--- components/navigation/Navigation.js
+++ components/navigation/Navigation.js
@@ -4,6 +4,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { Home } from "./Home";
 import { Details } from "./Details";
 import Signin from "../auth/Signin";
+import Signup from "../auth/Signup";
 const { Navigator, Screen } = createStackNavigator();
 
 const HomeNavigator = () => (
@@ -11,6 +12,7 @@ const HomeNavigator = () => (
     <Screen name="Home" component={Home} />
     <Screen name="Details" component={Details} />
     <Screen name="Signin" component={Signin} />
+    <Screen name="Signup" component={Signup} />
   </Navigator>
 );
 
