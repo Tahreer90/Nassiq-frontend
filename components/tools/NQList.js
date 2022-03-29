26d06480b06de4570f5e63d757bcd6d9e38450da
fatemah-alm <fatemah.alm@hotmail.com>
2022-03-29 20:49:56 +0300

    task edit fix

--- components/tools/NQList.js
+++ components/tools/NQList.js
@@ -93,23 +93,33 @@ const NQList = ({ group }) => {
               }
             ></CheckBox>
             {task.isChecked ? (
-              <Text
-                key={task._id}
-                style={{
-                  fontSize: 20,
-                  fontWeight: "500",
-                  width: 190,
-                  backgroundColor: "#242D65",
-                  color: "#C5C5C5",
-                  opacity: 1,
-                  marginBottom: 1,
-                  height: 30,
-                  top: 3,
-                  textDecorationLine: "line-through",
-                }}
-              >
-                {task.name}
-              </Text>
+              <>
+                <Text
+                  key={task._id}
+                  style={{
+                    fontSize: 20,
+                    fontWeight: "500",
+                    width: 190,
+                    backgroundColor: "#242D65",
+                    color: "#C5C5C5",
+                    opacity: 1,
+                    marginBottom: 1,
+                    height: 30,
+                    top: 3,
+                    textDecorationLine: "line-through",
+                  }}
+                >
+                  {task.name}
+                </Text>
+                {task.type == "High" && (
+                  <MaterialIcons
+                    name="priority-high"
+                    size={24}
+                    color="#FD6B68"
+                    style={{ position: "absolute", right: 27 }}
+                  />
+                )}
+              </>
             ) : (
               <View
                 style={{
@@ -123,7 +133,7 @@ const NQList = ({ group }) => {
                     fontSize: 20,
                     fontWeight: "500",
                     width: 190,
-                    backgroundColor: "red", // "#242D65",
+                    backgroundColor: "#242D65",
                     color: "#FFFFFF",
                     marginBottom: 1,
                     height: 30,
@@ -136,6 +146,13 @@ const NQList = ({ group }) => {
                 >
                   {task.name}
                 </Text>
+                {task.type == "High" && (
+                  <MaterialIcons
+                    name="priority-high"
+                    size={24}
+                    color="#FD6B68"
+                  />
+                )}
               </View>
             )}
 
